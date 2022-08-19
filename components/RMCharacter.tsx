b6c07438d52d4ed952a5497d260c590e7fb6fb3d
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-08-19 20:26:12 -0300

    update scroll view

--- components/RMCharacter.tsx
+++ components/RMCharacter.tsx
@@ -3,6 +3,7 @@ import {
   Text,
   Image,
   Pressable,
+  Dimensions,
   StyleSheet,
   SafeAreaView,
   ScrollView
@@ -30,23 +31,79 @@ function RMCharacter() {
   }, [])
 
   return(
-    <SafeAreaView>
+    <SafeAreaView
+      style={{ backgroundColor: '#7B25F0' }}
+    >
       <ScrollView>
-
-        { character?.map(
-          (item, index) => (
-            <View key={index}>
-              <Image 
-                style={{ width: 100, height: 100 }}
-                source={{ uri: item.image }}
-              />
-              <Text>{item.name}</Text>
-            </View>
-          )
-        ) }
+        <View
+          style={styles.container}
+        >
+          { character?.map(
+            (item, index) => (
+              <View 
+                style={styles.card}
+                key={index}
+              >
+                <Image 
+                  style={{ width: 100, height: 100 }}
+                  source={{ uri: item.image }}
+                />
+                <View
+                  style={styles.textBox}
+                >
+                  <Text
+                    style={styles.textName}
+                  >
+                    {item.name}
+                  </Text>
+                  <Text
+                    style={styles.text}
+                  >
+                    {item.species}
+                  </Text>
+                  <Text
+                    style={styles.text}
+                  >
+                    {item.gender}
+                  </Text>
+                </View>
+              </View>
+            )
+          ) }
+          </View>
       </ScrollView>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: Dimensions.get('window').width,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eee'
+  },
+  card: {
+    flexDirection: 'row',
+    backgroundColor: '#7B25F0',
+    width: Dimensions.get('window').width - 40,
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    borderRadius: 12,
+    margin: 12
+  },
+  textBox: {
+    flex: 1,
+    paddingHorizontal: 15
+  },
+  textName: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  text: {
+    color: '#fff'
+  },
+})
 
 export default RMCharacter
